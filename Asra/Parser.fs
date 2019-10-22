d87c5e7c83544bca48a3c4cf13e5f60ec97466b6
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2019-10-22 14:50:36 +0200

    Literal parser

--- Asra/Parser.fs
+++ Asra/Parser.fs
@@ -3,3 +3,44 @@
 open Ast
 open FParsec
 open FParsec.CharParsers
+
+let createParser (dataParser: Parser<'data, unit>) =
+    
+    let floatLiteralParser: Parser<Literal, unit> = numberLiteral (NumberLiteralOptions.DefaultFloat) "Float literal" |>> fun f -> 
+        match f.IsInteger with
+            | true -> int64 f.String |> Int
+            | false -> float f.String |> Float
+    
+    let intLiteralParser: Parser<Literal, unit> = pint64 |>> Int
+    
+    let unitLiteralParser: Parser<Literal, unit> = skipString "()" |>> fun _ -> Unit
+    
+    let unescapedCharParser: Parser<char, unit> = satisfy (fun ch -> ch <> '\\' && ch <> '\"')
+    
+    let escapedCharParser: Parser<char, unit> = 
+        [ 
+        "\\\"",'\"'
+        "\\\\",'\\'
+        "\\/",'/' 
+        "\\b",'\b'
+        "\\f",'\f'
+        "\\n",'\n'
+        "\\r",'\r'
+        "\\t",'\t'
+        ] 
+        |> List.map (fun (toMatch, result) -> skipString toMatch >>% result)
+        |> choice
+    
+    let stringCharParser: Parser<char, unit> = unescapedCharParser <|> escapedCharParser
+    
+    let stringLiteralParser: Parser<Literal, unit> = skipChar '"' >>. (manyChars stringCharParser) .>> skipChar '"' |>> String
+    
+    let literalExpressionParser: Parser<Expression<'data>, unit> = 
+        dataParser .>>.
+        choiceL [ 
+            intLiteralParser
+            floatLiteralParser
+            stringLiteralParser
+            unitLiteralParser ] "Literal" |>> fun (data, lit) -> Literal (lit, data)
+
+    literalExpressionParser
