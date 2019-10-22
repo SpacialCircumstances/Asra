5036e06544c643121a5673a397bffdb10444838a
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2019-10-22 12:01:16 +0200

    Add group and literal expressions

--- Asra/Ast.fs
+++ Asra/Ast.fs
@@ -12,7 +12,15 @@ type Declaration =
 
 type LetBinding<'data> = Declaration * Expression<'data>
 
+and Literal =
+    | Int of int64
+    | String of string
+    | Float of float
+    | Unit
+
 and Expression<'data> =
+    | Literal of Literal * 'data
+    | Group of Expression<'data>
     | Let of LetBinding<'data> list * Expression<'data> * 'data
     | Variable of string * 'data
     | Lambda of Declaration list * Expression<'data> * 'data
