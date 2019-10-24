c70f0938a7a0b2a9b4e121bc997ed4f696ae7ad1
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2019-10-24 14:53:53 +0200

    Improved parser labels

--- Asra/Parser.fs
+++ Asra/Parser.fs
@@ -18,18 +18,18 @@ let createParser (dataParser: Parser<'data, unit>) (logger: (string -> unit) opt
 
     let (functionExpressionParser: Parser<Expression<'data>, unit>, functionExpressionParserRef) = createParserForwardedToRef ()
 
-    let floatLiteralParser: Parser<Literal, unit> = numberLiteral (NumberLiteralOptions.DefaultFloat) "Float literal" |>> fun f -> 
+    let floatLiteralParser: Parser<Literal, unit> = numberLiteral (NumberLiteralOptions.DefaultFloat) "Float literal" |>> (fun f -> 
         match f.IsInteger with
             | true -> int64 f.String |> Int
-            | false -> float f.String |> Float
+            | false -> float f.String |> Float)
     
     let intLiteralParser: Parser<Literal, unit> = pint64 |>> Int
 
-    let mapString (str: string) (res: 'a): Parser<'a, unit> = (skipString str |>> fun _ -> res) <?> str
+    let mapString (str: string) (res: 'a): Parser<'a, unit> = (skipString str >>% res) <?> str
     
-    let boolLiteralParser: Parser<Literal, unit> = (mapString "false" false <|> mapString "true" true) |>> Bool
+    let boolLiteralParser: Parser<Literal, unit> = ((mapString "false" false <|> mapString "true" true) |>> Bool) <?> "Bool literal"
     
-    let unitLiteralParser: Parser<Literal, unit> = skipString "()" |>> fun _ -> Unit
+    let unitLiteralParser: Parser<Literal, unit> = mapString "()" Unit <?> "Unit literal"
     
     let unescapedCharParser: Parser<char, unit> = satisfy (fun ch -> ch <> '\\' && ch <> '\"')
     
@@ -49,7 +49,7 @@ let createParser (dataParser: Parser<'data, unit>) (logger: (string -> unit) opt
     
     let stringCharParser: Parser<char, unit> = unescapedCharParser <|> escapedCharParser
     
-    let stringLiteralParser: Parser<Literal, unit> = skipChar '"' >>. (manyChars stringCharParser) .>> skipChar '"' |>> String
+    let stringLiteralParser: Parser<Literal, unit> = (skipChar '"' >>. (manyChars stringCharParser) .>> skipChar '"' |>> String) <?> "String literal"
     
     let literalExpressionParser: Parser<Expression<'data>, unit> = 
         dataParser .>>.
@@ -58,9 +58,9 @@ let createParser (dataParser: Parser<'data, unit>) (logger: (string -> unit) opt
             intLiteralParser
             stringLiteralParser
             boolLiteralParser
-            unitLiteralParser ] "Literal" |>> (fun (data, lit) -> Literal (lit, data)) <!> "Literal expression parser"
+            unitLiteralParser ] "Literal" |>> (fun (data, lit) -> Literal (lit, data)) <?> "Literal expression" <!> "Literal expression parser"
 
-    let groupExpressionParser: Parser<Expression<'data>, unit> = dataParser .>> skipChar '(' .>>? spaces .>>. expressionParser .>> spaces .>> skipChar ')' |>> fun (data, expr) -> Group (expr, data)
+    let groupExpressionParser: Parser<Expression<'data>, unit> = (dataParser .>> skipChar '(' .>>? spaces .>>. expressionParser .>> spaces .>> skipChar ')' |>> fun (data, expr) -> Group (expr, data)) <?> "Group expression" <!> "Group expression parser"
 
     let isSeparator (c: char) = System.Char.IsWhiteSpace c || c = ')' || c = '('
     
@@ -84,32 +84,32 @@ let createParser (dataParser: Parser<'data, unit>) (logger: (string -> unit) opt
             | "end" -> fail ""
             | "else" -> fail ""
             | "import!" -> fail ""
-            | _ -> preturn s)
+            | _ -> preturn s) <?> "Identifier"
 
     let isSimpleWhitespace (c: char) = c = ' ' || c = '\t'
 
-    let ws1 = skipMany1Satisfy isSimpleWhitespace
+    let ws1 = skipMany1Satisfy isSimpleWhitespace <?> "Whitespace"
 
-    let declarationParser = nameParser |>> Named <!> "Declaration parser"
+    let declarationParser = nameParser |>> Named <?> "Declaration" <!> "Declaration parser"
 
     let keyword (kw: string) = skipString kw <?> kw <!> (sprintf "%s parser" kw)
 
-    let variableExpressionParser: Parser<Expression<'data>, unit> = dataParser .>>. nameParser |>> (fun (data, name) -> Variable (name, data)) <!> "Variable expression parser"
+    let variableExpressionParser: Parser<Expression<'data>, unit> = dataParser .>>. nameParser |>> (fun (data, name) -> Variable (name, data)) <?> "Variable expression" <!> "Variable expression parser"
 
-    let lambdaExpressionParser: Parser<Expression<'data>, unit> = dataParser .>> keyword "fun" .>>? spaces1 .>>.? (sepEndBy1 declarationParser spaces1) .>>? keyword "->" .>> spaces1 .>>. expressionParser |>> (fun ((data, parameters), expr) -> Lambda (parameters, expr, data)) <!> "Lambda expression parser"
+    let lambdaExpressionParser: Parser<Expression<'data>, unit> = dataParser .>> keyword "fun" .>>? spaces1 .>>.? (sepEndBy1 declarationParser spaces1) .>>? keyword "->" .>> spaces1 .>>. expressionParser |>> (fun ((data, parameters), expr) -> Lambda (parameters, expr, data)) <?> "Lambda expression" <!> "Lambda expression parser"
 
     let endParser = keyword "end"
 
-    let bindingParser: Parser<LetBinding<'data>, unit> = declarationParser .>> spaces1 .>> skipChar '=' .>> spaces1 .>>. expressionParser |>> LetBinding <!> "Let binding parser"
+    let bindingParser: Parser<LetBinding<'data>, unit> = declarationParser .>> spaces1 .>> skipChar '=' .>> spaces1 .>>. expressionParser |>> LetBinding  <?> "Let binding" <!> "Let binding parser"
 
-    let letParser: Parser<Expression<'data>, unit> = dataParser .>> keyword "let" .>>? spaces1 .>>. (sepEndBy1 bindingParser spaces1) .>> keyword "in" .>> spaces1 .>>. expressionParser .>> spaces1 .>> endParser |>> (fun ((data, bindings), expr) -> Let (bindings, expr, data)) <!> "Let expression parser"
+    let letParser: Parser<Expression<'data>, unit> = dataParser .>> keyword "let" .>>? spaces1 .>>. (sepEndBy1 bindingParser spaces1) .>> keyword "in" .>> spaces1 .>>. expressionParser .>> spaces1 .>> endParser |>> (fun ((data, bindings), expr) -> Let (bindings, expr, data)) <?> "Let expression" <!> "Let expression parser"
 
-    let importParser: Parser<Expression<'data>, unit> = dataParser .>> keyword "import!" .>> spaces1 .>>. nameParser |>> (fun (data, name) -> Import (name, data)) <!> "Import expression parser"
+    let importParser: Parser<Expression<'data>, unit> = dataParser .>> keyword "import!" .>> spaces1 .>>. nameParser |>> (fun (data, name) -> Import (name, data)) <?> "Import expression" <!> "Import expression parser"
 
-    let ifParser: Parser<Expression<'data>, unit> = dataParser .>> keyword "if" .>> spaces1 .>>. functionExpressionParser .>> spaces1 .>> keyword "then" .>> spaces1 .>>. expressionParser .>> spaces1 .>> keyword "else" .>> spaces1 .>>. expressionParser .>> spaces1 .>> endParser |>> (fun (((data, condExpr), ifBodyExpr), elseBodyExpr) -> If (condExpr, ifBodyExpr, elseBodyExpr, data)) <!> "If expression parser"
+    let ifParser: Parser<Expression<'data>, unit> = dataParser .>> keyword "if" .>> spaces1 .>>. functionExpressionParser .>> spaces1 .>> keyword "then" .>> spaces1 .>>. expressionParser .>> spaces1 .>> keyword "else" .>> spaces1 .>>. expressionParser .>> spaces1 .>> endParser |>> (fun (((data, condExpr), ifBodyExpr), elseBodyExpr) -> If (condExpr, ifBodyExpr, elseBodyExpr, data)) <?> "If expression" <!> "If expression parser"
 
     //TODO: Remove restriction that function calls must happen on one line?
-    let functionCallParser: Parser<Expression<'data>, unit> = dataParser .>>. functionExpressionParser .>>? ws1 .>>.? attempt (sepBy1 functionExpressionParser ws1) |>> (fun ((data, funExpr), argExprs) -> FunctionCall (funExpr, argExprs, data)) <!> "Function call expression parser"
+    let functionCallParser: Parser<Expression<'data>, unit> = dataParser .>>. functionExpressionParser .>>? ws1 .>>.? attempt (sepBy1 functionExpressionParser ws1) |>> (fun ((data, funExpr), argExprs) -> FunctionCall (funExpr, argExprs, data)) <?> "Function call" <!> "Function call expression parser"
 
     expressionParserRef := choiceL [
         letParser
