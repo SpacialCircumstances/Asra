40419e0eaa80a7febe1bc491abfdc69e361d87e8
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2019-10-28 08:59:53 +0100

    Build exitable multiline input for REPL

--- Asra.Compiler/Repl.fs
+++ Asra.Compiler/Repl.fs
@@ -1,10 +1,41 @@
 ﻿module Repl
 
+open System
+
 type Arguments = {
     printAst: bool
     printIR: bool
     printTIR: bool
 }
 
+let readCode () =
+    let mutable reading = true
+    let mutable lle = false
+    let lines = ResizeArray()
+    while reading do
+        Console.Write("> ")
+        let line: string = Console.ReadLine()
+        lines.Add line
+        if String.IsNullOrWhiteSpace line then
+            if lle then
+                reading <- false
+            else
+                lle <- true
+        else
+            lle <- false
+     
+    String.Join("\n", lines)
+
+let runCode (args: Arguments) (code: string) =
+    ()
+
 let runRepl (args: Arguments) =
+    let run = runCode args
+    let mutable running = true
+    while running do
+        let code = readCode ()
+        match code.Trim() with
+            | ":q" | ":quit" | ":exit" -> running <- false
+            | _ -> run code
+                
     ()
