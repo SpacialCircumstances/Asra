3525a4ab3ca4192229483311ad62bd66d16f1380
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2019-10-28 19:45:13 +0100

    Add AST of IR

---
+++ Asra/IR.fs
@@ -0,0 +1,24 @@
+﻿module IR
+
+type Literal =
+    | Int of int64
+    | String of string
+    | Float of float
+    | Bool of bool
+    | Unit
+
+type LetExpression<'data> = {
+    varName: string
+    value: Expression<'data>
+    body: Expression<'data>
+    data: 'data
+}
+
+and Expression<'data> =
+    | Literal of Literal * 'data
+    | Variable of string * 'data
+    | Application of Expression<'data> * Expression<'data> * 'data
+    | Lambda of string * Expression<'data> * 'data
+    | Let of LetExpression<'data>
+    | LetRec of LetExpression<'data>
+    | If of Expression<'data> * Expression<'data> * Expression<'data> * 'data
