7a3ca4272b4b74ea183cd524320d0da89d3da29e
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2019-10-29 12:10:29 +0100

    IR generation for literals, variables and groups

--- Asra/IRGenerator.fs
+++ Asra/IRGenerator.fs
@@ -1,4 +1,20 @@
 ﻿module IRGenerator
 
+open AstCommon
+
 let rec map (expr: FrontendAst.Expression<'data>): IR.Expression<'data> =
-    invalidOp ""
+    let mapLit (lit: Literal<FrontendAst.Expression<'data>>) =
+        match lit with
+            | List exprs ->
+                List (List.map map exprs)
+            | Unit -> Unit
+            | Int i -> Int i
+            | String s -> String s
+            | Float f -> Float f
+            | Bool b -> Bool b
+
+    match expr with
+        | FrontendAst.Literal (lit, data) -> IR.Literal (mapLit lit, data)
+        | FrontendAst.Group (expr, _) -> map expr
+        | FrontendAst.Variable (name, data) -> IR.Variable (name, data)
+        | _ -> invalidOp "Not implemented"
