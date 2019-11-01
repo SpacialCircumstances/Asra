546deaedf5dc8accd205328399fd509dd4ef8f42
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2019-11-01 16:49:47 +0100

    Add test for let IR generation

--- Asra.Test/IRGeneration.fs
+++ Asra.Test/IRGeneration.fs
@@ -16,3 +16,25 @@ let ``Lambda`` () =
                     IR.Lambda(Named "c", 
                         IR.Variable ("a", ()), ()), ()), ())
     assertEq ir (IRGenerator.map ast)
+
+[<Fact>]
+let ``Let`` () =
+    let ast = FrontendAst.Let([
+        None, Named "x", FrontendAst.Literal (Int 2L, ())
+        Some FrontendAst.Recursive, Named "d", FrontendAst.Lambda ([
+            Named "a"
+            Named "b"
+        ], FrontendAst.Group(FrontendAst.Variable("a", ()), ()), ())
+    ], FrontendAst.Variable("t", ()), ())
+    let ir = IR.Let ({
+        data = ()
+        binding = Named "x"
+        value = IR.Literal (Int 2L, ())
+        body = IR.LetRec({
+            data = ()
+            binding = Named "d"
+            value = IR.Lambda (Named "a", IR.Lambda(Named "b", IR.Variable("a", ()), ()), ())
+            body = IR.Variable("t", ())
+        })
+    })
+    assertEq ir (IRGenerator.map ast)
