8de3b9d11e144150a50a32023b8d22fbccd7bcd0
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2019-11-11 18:52:58 +0100

    Add typechecking to CLI compiler

--- Asra.Compiler/Compiler.fs
+++ Asra.Compiler/Compiler.fs
@@ -18,7 +18,16 @@ let runCompiler (args: Arguments) =
                 args.astPrint.WriteLine (sprintf "%A" ast)
                 let ir = IRGenerator.map ast
                 args.irPrint.WriteLine (sprintf "%A" ir)
-                Ok "Compilation finished"
+                match Typechecker.generateTypenames ir with
+                    | Ok typedIR -> 
+                        args.tirPrint.WriteLine (sprintf "%A" typedIR)
+                        let eqs = Typechecker.generateEquations typedIR
+                        let subst = Typechecker.unifyAll eqs
+                        args.tirPrint.WriteLine (sprintf "%A" eqs)
+                        args.tirPrint.WriteLine (sprintf "%A" subst)
+                        Ok "Compilation finished"
+                    | Error ter ->
+                        Error (sprintf "Error: %s" ter)
             | Error parserError ->
                 Error (sprintf "Error: %s" parserError)
     else
