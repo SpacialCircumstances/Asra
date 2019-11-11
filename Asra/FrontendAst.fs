78fb9e7d861e6160998649240fbe7b473eef4368
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2019-11-11 22:41:23 +0100

    Improved frontend AST printing

--- Asra/FrontendAst.fs
+++ Asra/FrontendAst.fs
@@ -13,7 +13,7 @@ with
 
 type LetBinding<'data> = (BindingModifier option) * Declaration * Expression<'data>
 
-and Expression<'data> =
+and [<StructuredFormatDisplayAttribute("{AsString}")>] Expression<'data> =
     | Literal of Literal<Expression<'data>> * 'data
     | Group of Expression<'data> * 'data
     | Let of LetBinding<'data> list * Expression<'data> * 'data
@@ -24,3 +24,23 @@ and Expression<'data> =
     | Import of string * 'data
     | UnaryOperatorCall of string * Expression<'data> * 'data
     | BinaryOperatorCall of Expression<'data> * string * Expression<'data> * 'data
+with
+    override self.ToString () =
+        let printBinding (modifier, decl, expr) = 
+            match modifier with
+                | Some modi -> sprintf "%A %A = %A" modi decl expr
+                | None -> sprintf "%A = %A" decl expr
+
+        match self with
+            | Literal (lit, data) -> sprintf "(Literal %A @%A)" lit data
+            | Group (expr, data) -> sprintf "(Group %A @%A)" expr data
+            | BinaryOperatorCall (op1, operator, op2, data) -> sprintf "(BinOp %s %A %A @%A)" operator op1 op2 data
+            | FunctionCall (funcExpr, argsExpr, data) -> sprintf "(Call %A [%s] @%A)" funcExpr (System.String.Join("; ", argsExpr)) data
+            | If (ifExpr, thenExpr, elseExpr, data) -> sprintf "(If %A then %A else %A @%A)" ifExpr thenExpr elseExpr data
+            | Import (name, data) -> sprintf "(Import %s @%A)" name data
+            | Lambda (decls, expr, data) -> sprintf "(Lambda [%s] %A @%A)" (System.String.Join(" ", decls)) expr data
+            | Let (bindings, expr, data) -> sprintf "(Let [%s] %A @%A)" (System.String.Join("; ", Seq.map printBinding bindings)) expr data
+            | UnaryOperatorCall (op, expr, data) -> sprintf "(UnOp %s %A @%A)" op expr data
+            | Variable (name, data) -> sprintf "%s@%A" name data
+
+    member self.AsString = self.ToString ()
