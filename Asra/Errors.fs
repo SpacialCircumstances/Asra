2fff7008868526900f479eea6191f825cd0d2ae0
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2019-12-07 12:34:35 +0100

    Add ReturnFrom so results can be returned directly

--- Asra/Errors.fs
+++ Asra/Errors.fs
@@ -9,6 +9,7 @@ let collectErrors (results: Result<'a, 'b> list): 'a list * 'b list =
 type ResultBuilder () =
     member this.Bind (r, f) = Result.bind f r
     member this.Return (x) = Ok x
+    member this.ReturnFrom (a) = a
     member this.Zero () = Ok ()
 
 let result = new ResultBuilder()
