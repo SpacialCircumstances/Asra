c5f1e53cad3b53d30e67aa24185a5d399a77bedd
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2019-12-25 15:30:10 +0100

    Add some basic functions to prelude

--- Asra/Prelude.fs
+++ Asra/Prelude.fs
@@ -2,6 +2,24 @@
 
 open AstCommon
 
+let private tunit = Name "Unit"
+let private tbool = Name "Bool"
+let private tint = Name "Int"
+let private tfloat = Name "Float"
+let private tstring = Name "String"
+
 let context: Map<string, TypeDeclaration> = Map.ofList [
-    "println", Function (Generic "a", Name "Unit")
+    "println", Function (Generic "a", tunit)
+    "==", Function (Generic "a", Function (Generic "a", tbool))
+    "-", Function (tint, Function (tint, tint))
+    "+", Function (tint, Function (tint, tint))
+    "*", Function (tint, Function (tint, tint))
+    "/", Function (tint, Function (tint, tint))
+    ".+", Function (tfloat, Function (tfloat, tfloat))
+    ".-", Function (tfloat, Function (tfloat, tfloat))
+    ".*", Function (tfloat, Function (tfloat, tfloat))
+    "./", Function (tfloat, Function (tfloat, tfloat))
+    "intToFloat", Function (tint, tfloat)
+    "floatToInt", Function (tfloat, tint)
+    "toString", Function (Generic "a", tstring)
 ]
