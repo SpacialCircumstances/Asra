221615aac6fb61702d23b082474c07bd7e7b09e2
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2019-12-26 20:08:09 +0100

    Begin defining IR for js backend

---
+++ Asra/JsBackend.fs
@@ -0,0 +1,18 @@
+﻿module JsBackend
+
+type Variable = string
+
+type Block = {
+    statements: Statement list
+    returnValue: Variable
+}
+
+and Value =
+    | Literal of AstCommon.Literal<Value>
+    | Lambda of Variable list * Block
+    | FunctionCall of Variable * Value list
+
+and Statement = 
+    | Import
+    | Assignment of Variable * Value
+    | ConditionalAssignment
