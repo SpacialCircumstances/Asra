98cc9fcb5c5600f17298ff3bbee0688fa98a5660
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2020-02-06 18:39:59 +0100

    Create module for a lexer token

---
+++ Asra/Token.fs
@@ -0,0 +1,41 @@
+﻿module Token
+
+[<StructuredFormatDisplay("{filename}:{line}:{col}")>]
+type SourcePosition = {
+    line: int
+    col: int
+    filename: string
+}
+
+type TokenData =
+    | StringLiteral of string
+    | NumberLiteral of string
+    | Identifier of string
+    | Let
+    | In
+    | Rec
+    | End
+    | If
+    | Then
+    | Else
+    | Fun
+    | Type
+    | Equal
+    | Arrow
+    | LeftParen
+    | RightParen
+    | LeftSquareBracket
+    | RightSquareBracket
+    | LeftCurlyBracket
+    | RightCurlyBracket
+    | Semicolon
+    | Comma
+    | Period
+    | Colon
+
+[<Struct>]
+type Token = {
+    data: TokenData
+    lexeme: string
+    position: SourcePosition
+}
