edcb4fc4bb18d368b125505ff29183bdf697d46a
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2020-02-12 17:31:31 +0100

    Add matcher for token tests

---
+++ Asra.Test/LexerTests.fs
@@ -0,0 +1,10 @@
+﻿module LexerTests
+
+open Xunit
+
+let tokenizedMatch code tokenData =
+    let t, e = Lexer.lexer "Test" code
+    Assert.Empty(e)
+    let data = Seq.map (fun (t: Token.Token) -> t.data) t |> Seq.toList
+    Assert.Equal<Token.TokenData list>(tokenData, data)
+
